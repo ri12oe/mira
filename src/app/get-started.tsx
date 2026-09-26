cb6644efcd2b9b6e86208cc0a4560f9e542d745a
Mario Espinoza <mario.switch100@gmail.com>
2026-09-26 09:51:15 -0400

    created the header

--- src/app/get-started.tsx
+++ src/app/get-started.tsx
@@ -1,6 +1,14 @@
 import { CornerClouds, useCornerCloudSize } from "@/components/conor_clouds";
-import { ScrollView, StyleSheet, View } from "react-native";
+import { ScrollView, StyleSheet, View, Text } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { AppText } from "@/components/app-text";
+
+//  New Colors to test out
+const lightGreen = "#A8D130";
+const darkBlue = "#1C2E9D";
+const lightBlue = "#414DC7";
+const lightPurple = "#B024F1";
+const lighterBlue = "#349AD5";
 
 export default function GetStarted() {
   const insets = useSafeAreaInsets();
@@ -15,6 +23,10 @@ export default function GetStarted() {
           paddingHorizontal: 23,
         }}
       >
+        <View style={styles.header}>
+            <AppText style={styles.headerText}>welcome to</AppText>
+            <AppText style={styles.headerBrand}>Mira</AppText>
+        </View>
         
       </ScrollView>
     </View>
@@ -23,7 +35,26 @@ export default function GetStarted() {
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
-    justifyContent: "center",
+    overflow: "hidden",
+    backgroundColor: "#FFF8FC",
+  },
+  header: {
     alignItems: "center",
+    justifyContent: "center",
+    gap: 13,
+    marginBottom: 24,
+  },
+  headerText: {
+    fontSize: 24,
+    color: darkBlue,
+    fontWeight: "500",
+    
+  },
+  headerBrand: {
+    fontSize: 120,
+    color: "black",
+    fontWeight: "800",
+    lineHeight: 96,
+    letterSpacing: 2,
   },
 });
